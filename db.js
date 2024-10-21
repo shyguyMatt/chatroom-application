5c16b68e1daba832293010475b832751f85f4ff1
ShyguyMatt <mjherford@gmail.com>
2024-10-21 10:25:44 -0400

    updated to use env file

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const db = createPool({
-  host: DBHOST,
-  user: DBUSER,
-  password: DBPASSWORD,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
   database: 'chatroomMessages'
 }).promise()
 
